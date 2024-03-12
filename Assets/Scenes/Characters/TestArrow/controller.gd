75931e6372165affb531a32f7fbaab2b16695726
TruPotato <80653172+TruPotato@users.noreply.github.com>
2024-03-11 18:41:59 -0700

    Added animation tree, begun attack code

--- Assets/Scenes/Characters/TestArrow/controller.gd
+++ Assets/Scenes/Characters/TestArrow/controller.gd
@@ -8,33 +8,17 @@ var DEBUG = true
 func _physics_process(delta):
 
 	var direction = Input.get_vector("left", "right", "up", "down")
-	var dirX = 0
-	var dirY = 0
 	if DEBUG:
-		dirX = Input.get_axis("left", "right")
-		dirY = Input.get_axis("up", "down")
+		var dirX = Input.get_axis("left", "right")
+		var dirY = Input.get_axis("up", "down")
 		$RichTextLabel.text = "[center]" + str(Vector2(dirX, dirY)) + "[/center]"
 	if direction:
 		velocity = velocity.move_toward(direction * SPEED, FRICTION * delta)
 
 		# animation shit
-		match Vector2(dirX, dirY):
-			Vector2(0, 1):
-				$AnimatedSprite2D.play("S")
-			Vector2(1, 1):
-				$AnimatedSprite2D.play("SE")
-			Vector2(1, 0):
-				$AnimatedSprite2D.play("E")
-			Vector2(1, -1):
-				$AnimatedSprite2D.play("NE")
-			Vector2(0, -1):
-				$AnimatedSprite2D.play("N")
-			Vector2(-1, -1):
-				$AnimatedSprite2D.play("NW")
-			Vector2(-1, 0):
-				$AnimatedSprite2D.play("W")
-			Vector2(-1, 1):
-				$AnimatedSprite2D.play("SW")
+		var animX = Input.get_axis("left", "right")
+		var animY = Input.get_axis("up", "down")
+		$Character/AnimationTree.set("parameters/Directions/blend_position", Vector2(animX, animY))
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
